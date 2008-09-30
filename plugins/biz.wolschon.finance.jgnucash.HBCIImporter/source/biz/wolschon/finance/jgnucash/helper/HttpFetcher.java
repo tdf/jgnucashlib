ff4be9ae59be1ec807fd965b9fb1ae5156eef1ce
marcuswolschon <marcuswolschon@d5cbeab5-833d-439b-9598-54350a4ff33e>
2008-09-30 16:18:43 +0000

    Given httpfetcher a proper logger
    git-svn-id: https://jgnucashlib.svn.sourceforge.net/svnroot/jgnucashlib@11 d5cbeab5-833d-439b-9598-54350a4ff33e

---
+++ plugins/biz.wolschon.finance.jgnucash.HBCIImporter/source/biz/wolschon/finance/jgnucash/helper/HttpFetcher.java
@@ -0,0 +1,121 @@
+/**
+ * OsCommerceOrderImporter.java
+ * Created on 27.07.2008
+ * (c) 2005 by "Wolschon Softwaredesign und Beratung".
+ *
+ *  Permission is granted to use, modify, publish and sub-license this code
+ *  as specified in the contract. If nothing else is specified these rights
+ *  are given non-exclusively with no restrictions solely to the contractor(s).
+ *  If no specified otherwise I reserve the right to use, modify, publish and
+ *  sub-license this code to other parties myself.
+ *
+ * -----------------------------------------------------------
+ * major Changes:
+ *  27.07.2008 - initial version
+ * ...
+ *
+ */
+package biz.wolschon.finance.jgnucash.helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+//import java.security.SecureRandom;
+//import java.security.Security;
+//import java.security.cert.CertificateException;
+//import java.security.cert.X509Certificate;
+//
+//import javax.net.ssl.HostnameVerifier;
+//import javax.net.ssl.HttpsURLConnection;
+//import javax.net.ssl.SSLContext;
+//import javax.net.ssl.TrustManager;
+//import javax.net.ssl.X509TrustManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * created: 27.07.2008 <br/>
+ *
+ * Helper-Class to getch a URL with HTTP-Basic-Auth.
+ *
+ * @author <a href="mailto:Marcus@Wolschon.biz">Marcus Wolschon</a>
+ */
+
+public class HttpFetcher {
+
+	/**
+	 * Automatically created logger for debug and error-output.
+	 */
+	private static final Log LOGGER = LogFactory.getLog(HttpFetcher.class);
+
+
+	/**
+	 * Fetch the given URL with http-basic-auth.
+	 * @param url the URL
+	 * @param username username
+	 * @param password password
+	 * @return the page-content or null
+	 */
+	public static String fetchURL (final URL url, final String username, final String password) {
+		StringWriter sw = new StringWriter();
+
+		try {
+			PrintWriter  pw = new PrintWriter(sw);
+			InputStream content = fetchURLStream(url, username, password);
+			BufferedReader in   = 
+				new BufferedReader (new InputStreamReader (content));
+			String line;
+			while ((line = in.readLine()) != null) {
+				pw.println (line);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			LOGGER.error("cannot fetch malformed URL " + url, e);
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			LOGGER.error("cannot fetch URL " + url, e);
+			return null;
+		}
+		return sw.toString();
+	}
+	
+	   /**
+     * Fetch the given URL with http-basic-auth.
+     * @param url the URL
+     * @param username username
+     * @param password password
+     * @return the page-content or null
+     */
+    public static InputStream fetchURLStream (final URL url, final String username, final String password) {
+
+        try {
+            String userPassword = username + ":" + password;
+
+            // Encode String
+            String encoding = new sun.misc.BASE64Encoder().encode (userPassword.getBytes());
+
+            URLConnection uc = url.openConnection();
+            uc.setRequestProperty  ("Authorization", "Basic " + encoding);
+            return (InputStream)uc.getInputStream();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            LOGGER.error("cannot fetch malformed URL " + url, e);
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error("cannot fetch URL " + url, e);
+            return null;
+        }
+    }
+
+
+}
