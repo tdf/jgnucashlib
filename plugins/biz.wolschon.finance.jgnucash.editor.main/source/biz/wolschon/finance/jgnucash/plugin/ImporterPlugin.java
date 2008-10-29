54ff01b2027f280d121fbdae51fc4f644623d4c2
marcuswolschon <marcuswolschon@d5cbeab5-833d-439b-9598-54350a4ff33e>
2008-10-29 16:01:00 +0000

    cleaned up whitespace
    git-svn-id: https://jgnucashlib.svn.sourceforge.net/svnroot/jgnucashlib@36 d5cbeab5-833d-439b-9598-54350a4ff33e

--- plugins/biz.wolschon.finance.jgnucash.editor.main/source/biz/wolschon/finance/jgnucash/plugin/ImporterPlugin.java
+++ plugins/biz.wolschon.finance.jgnucash.editor.main/source/biz/wolschon/finance/jgnucash/plugin/ImporterPlugin.java
@@ -17,25 +17,25 @@ import biz.wolschon.fileformats.gnucash.GnucashWritableFile;
  * Extension - point declaration:<br/>
  * <pre>
  * <extension-point id="Importer">
-		<parameter-def id="class" /> (classname of class implementing this interface)
-		<parameter-def id="name" /> (name to be displayed in menus,...)
-		<parameter-def id="hasFixedAccount" /> <!-- false=use currently selected account -->
-		<parameter-def id="description" multiplicity="none-or-one" /> (optionsl, very short description)
-		<parameter-def id="icon" multiplicity="none-or-one" /> (optional path to icon-ressource)
-	</extension-point>
-	</pre>
- * 
+        <parameter-def id="class" /> (classname of class implementing this interface)
+        <parameter-def id="name" /> (name to be displayed in menus,...)
+        <parameter-def id="hasFixedAccount" /> <!-- false=use currently selected account -->
+        <parameter-def id="description" multiplicity="none-or-one" /> (optionsl, very short description)
+        <parameter-def id="icon" multiplicity="none-or-one" /> (optional path to icon-ressource)
+    </extension-point>
+    </pre>
+ *
  * @author <a href="mailto:Marcus@Wolschon.biz">Marcus Wolschon</a>
  *
  */
 public interface ImporterPlugin {
 
-	/**
-	 * Runt the actual import.
-	 * @param writableModel the gnucash-file to import to.
-	 * @param currentAccount the currently selected account (may be null)
-	 * @return (optional) a message to be displayed after success (or null/empty message)
-	 */
-	String runImport(final GnucashWritableFile writableModel, final GnucashWritableAccount currentAccount) throws Exception;
+    /**
+     * Runt the actual import.
+     * @param writableModel the gnucash-file to import to.
+     * @param currentAccount the currently selected account (may be null)
+     * @return (optional) a message to be displayed after success (or null/empty message)
+     */
+    String runImport(final GnucashWritableFile writableModel, final GnucashWritableAccount currentAccount) throws Exception;
 
 }
