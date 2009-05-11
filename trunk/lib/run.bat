3c926a9d139b466999ffb39b8f9bd406711099a6
MarcusWolschon <MarcusWolschon@d5cbeab5-833d-439b-9598-54350a4ff33e>
2009-05-11 04:25:54 +0000

    note about java1.6
    git-svn-id: https://jgnucashlib.svn.sourceforge.net/svnroot/jgnucashlib@149 d5cbeab5-833d-439b-9598-54350a4ff33e

--- trunk/lib/run.bat
+++ trunk/lib/run.bat
@@ -1,2 +1,4 @@
+echo "Note: please run this only from the directory where jGnucash is installed"
+echo "and only with Java 1.6 or newer"
 START javaw -jar lib\jpf-boot.jar
 REM pause
--- trunk/lib/run.sh
+++ trunk/lib/run.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
+#!/bin/sh
+echo "Note: please run this only from the directory where jGnucash is installed"
+echo "and only with Java 1.6 or newer"
 java -jar lib/jpf-boot.jar 
