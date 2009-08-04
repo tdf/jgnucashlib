8d71ee4dc850037faa41ad3bf653470875d68d78
MarcusWolschon <MarcusWolschon@d5cbeab5-833d-439b-9598-54350a4ff33e>
2009-08-04 05:08:33 +0000

    cleaned up and compiles
    git-svn-id: https://jgnucashlib.svn.sourceforge.net/svnroot/jgnucashlib@201 d5cbeab5-833d-439b-9598-54350a4ff33e

--- trunk/jGnucashLib/plugins/biz.wolschon.finance.jgnucash.mysql/src/biz/wolschon/finance/jgnucash/mysql/MySQLDataSource.java
+++ trunk/jGnucashLib/plugins/biz.wolschon.finance.jgnucash.mysql/src/biz/wolschon/finance/jgnucash/mysql/MySQLDataSource.java
@@ -29,39 +29,19 @@
  *   as a regression-test.
  */
 
-package biz.wolschon.finance.jgnucash.ssh;
+package biz.wolschon.finance.jgnucash.mysql;
 
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.xml.bind.JAXBException;
 
 import biz.wolschon.fileformats.gnucash.GnucashWritableFile;
-import biz.wolschon.fileformats.gnucash.jwsdpimpl.GnucashFileWritingImpl;
 import biz.wolschon.finance.jgnucash.plugin.DataSourcePlugin;
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UIKeyboardInteractive;
-import com.jcraft.jsch.UserInfo;
 
 /**
  * (c) 2009 by <a href="http://Wolschon.biz>Wolschon Softwaredesign und Beratung</a>.<br/>
@@ -91,120 +71,20 @@ public class MySQLDataSource implements DataSourcePlugin {
      */
     @Override
     public GnucashWritableFile loadFile() throws IOException, JAXBException {
-
-//        SSHDialog dialog = new SSHDialog();
-//        dialog.setVisible(true);
-//        //String input = JOptionPane.showInputDialog("Please enter the file to load.", "user@host:path/file");
-//        File tempFile = File.createTempFile("jGnucaashEditor_SSH_", ".xml.gz");
-//        if (loadFileeViaSSH(dialog.getRemoteHostUserInfo(), dialog.getSSHTunnelUserInfo(), dialog.getRemotePath(), tempFile)) {
-//            myLastLoadedFile = dialog;
-//            return new GnucashFileWritingImpl(tempFile);
-//        }
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost/gnucash", "root", "");
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Cannot open database-connection", e);
+        }
         return null;
     }
 
-    /**
-     * @param remoteSystem
-     * @param sshTunnelSystem
-     * @return
-     * @throws JSchException
-     */
-    private Session createConnection(final ConnectInfo remoteSystem,
-                                     final ConnectInfo sshTunnelSystem)
-                                                                       throws JSchException {
-        JSch jsch = new JSch();
-        Session session = null;
-        if (sshTunnelSystem != null) {
-            Session tunnelSession = jsch.getSession(sshTunnelSystem.getUserName(), sshTunnelSystem.getHostName(), sshTunnelSystem.getHostPost());
-            SSHUserInfo ui = new SSHUserInfo();
-            ui.setPassword(sshTunnelSystem.getPassword());
-            tunnelSession.setUserInfo(ui);
-            tunnelSession.connect();
-            int assingedPort = tunnelSession.setPortForwardingL(8764, remoteSystem.getHostName(), remoteSystem.getHostPost());
-            // connect through tunnel
-            // TODO: try ::1 for IPv6 if there is no 127.0.0.1
-            session = jsch.getSession(remoteSystem.getUserName(), "127.0.0.1", assingedPort);
-        } else {
-            // connect directly
-            session = jsch.getSession(remoteSystem.getUserName(), remoteSystem.getHostName(), remoteSystem.getHostPost());
-        }
-        // username and password will be given via UserInfo interface.
-        SSHUserInfo ui = new SSHUserInfo();
-        ui.setPassword(remoteSystem.getPassword());
-        session.setUserInfo(ui);
-        session.connect();
-
-        return session;
-    }
-
-
-    static int checkAck(final InputStream in) throws IOException{
-        int b = in.read();
-        // b may be 0 for success,
-        //          1 for error,
-        //          2 for fatal error,
-        //          -1
-        if (b == 0) {
-            return b;
-        }
-        if (b == -1) {
-            return b;
-        }
-
-        if (b==1 || b==2) {
-            StringBuffer sb = new StringBuffer();
-            int c;
-            do {
-                c = in.read();
-                sb.append((char)c);
-            }
-            while (c != '\n');
-            if  (b == 1) { // error
-                System.out.print(sb.toString());
-            }
-            if (b == 2) { // fatal error
-                System.out.print(sb.toString());
-            }
-        }
-        return b;
-    }
-
-    public static class SSHUserInfo implements UserInfo, UIKeyboardInteractive{
-        public boolean promptYesNo(String str){
-            Object[] options={ "yes", "no" };
-            int foo=JOptionPane.showOptionDialog(null,
-                    str,
-                    "Warning",
-                    JOptionPane.DEFAULT_OPTION,
-                    JOptionPane.WARNING_MESSAGE,
-                    null, options, options[0]);
-            return foo==0;
-        }
-
-        private String myPassword;
-        JTextField passwordField=new JPasswordField(20);
-
-        public String getPassphrase(){ return null; }
-        public boolean promptPassphrase(String message){ return true; }
-        public boolean promptPassword(String message){
-            Object[] ob={passwordField};
-            int result=
-                JOptionPane.showConfirmDialog(null, ob, message,
-                        JOptionPane.OK_CANCEL_OPTION);
-            if(result==JOptionPane.OK_OPTION){
-                myPassword=passwordField.getText();
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-    /* (non-Javadoc)
+     /* (non-Javadoc)
      * @see biz.wolschon.finance.jgnucash.plugin.DataSourcePlugin#write(biz.wolschon.fileformats.gnucash.GnucashWritableFile)
      */
     @Override
-    public void write(final GnucashWritableFile aFile) throws IOException,
-//    JAXBException {
+    public void write(final GnucashWritableFile aFile) throws IOException, JAXBException {
 //        if (myLastLoadedFile == null) {
 //            return;
 //        }
